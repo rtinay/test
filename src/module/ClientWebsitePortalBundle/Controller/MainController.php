96868c44a4b85f223679496b29f6aace237e3701
tinay <tinay.ramala@gmail.com>
2016-10-17 11:27:33 +0300

    login, register template

--- src/module/ClientWebsitePortalBundle/Controller/MainController.php
+++ src/module/ClientWebsitePortalBundle/Controller/MainController.php
@@ -6,6 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MainController extends Controller {
     public function homePageAction() {
+
+	    if (!$this->get('security.authorization_checker')->isGranted('ROLE_USER')) {
+	      throw new AccessDeniedException('Accès limité aux auteurs.');
+	    }
         return $this->render('moduleClientWebsitePortalBundle:HomePage:homepage.html.twig', 
         						array('page_title'=>'Home Page')
         					);
